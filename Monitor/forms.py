dba273632d151186cb6dac6426a0c64faa995073
Marco <marco.torre92@alice.it>
2016-04-13 17:53:15 +0200

    introdotte funzioni generatrici

--- Monitor/forms.py
+++ Monitor/forms.py
@@ -6,10 +6,7 @@ def ottieni_comuni():
     """
     Seleziona i comuni e le formatta per essere passati come scelta al form
     """
-    comuni = Comune.objects.all()
-    choices = []
-    for comune in comuni:
-        choices.append((comune.id, comune.nome))
+    choices = [(comune.id, comune.nome)for comune in Comune.objects.all()]
     return choices
 
 
@@ -25,18 +22,12 @@ class SceltaComuni(forms.Form):
 
 
 def ottieni_frazioni(id_comuni):
-    comuni = []
-    for x in id_comuni:
-        y = Comune.objects.filter(id=x).last()
-        comuni.append(y)
+    comuni = [Comune.objects.filter(pk=id_comune) for id_comune in id_comuni]
     frazioni = []
     for comune in comuni:
         y = Frazione.objects.filter(comune=comune)
         frazioni += y
-    choices = []
-
-    for frazione in frazioni:
-        choices.append((frazione.id, frazione.nome + " - " + frazione.comune.nome))
+    choices = [(frazione.id, frazione.nome + " - " + frazione.comune.nome) for frazione in frazioni]
     return choices
 
 
@@ -57,18 +48,17 @@ class SceltaFrazioni (forms.Form):
 
 
 def ottieni_monitor(id_frazioni):
-    frazioni = []
-    for x in id_frazioni:
-        y = Frazione.objects.filter(id=x).last
-        frazioni.append(y)
+    """
+    Funzione per la selezione dei monitor appartenenti a una serie di frazioni
+    :param id_frazioni: una lista di id di frazioni
+    :return: una lista di monitor associati
+    """
+    frazioni = [Frazione.objects.filter(pk=id_frazione) for id_frazione in id_frazioni]
     monitor = []
     for frazione in frazioni:
         y = Monitor.objects.filter(frazione_posizionamento=frazione)
         monitor += y
-    choices = []
-
-    for m in monitor:
-        choices.append((m.id, m.nome + " - " + m.frazione_posizionamento.nome))
+    choices = [(m.id, m.nome + " - " + m.frazione_posizionamento.nome) for m in monitor]
     return choices
 
 
